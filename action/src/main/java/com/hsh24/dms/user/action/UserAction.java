1f0824615a258a737289b93dea456e3a9b09b002
Jiakun Xu <jiakunxu@gmail.com>
2016-05-16 17:16:23 +0800

    我的信息

---
+++ action/src/main/java/com/hsh24/dms/user/action/UserAction.java
@@ -0,0 +1,22 @@
+package com.hsh24.dms.user.action;
+
+import com.hsh24.dms.framework.action.BaseAction;
+
+/**
+ * 
+ * @author JiakunXu
+ * 
+ */
+public class UserAction extends BaseAction {
+
+	private static final long serialVersionUID = 7825329647617901909L;
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String detail() {
+		return SUCCESS;
+	}
+
+}
