c6df74f35e86c2f19d2befe17a261222ccf015f9
Jiakun Xu <jiakunxu@gmail.com>
2016-05-27 17:30:34 +0800

    <>?

--- web/src/main/webapp/statics/js/cashflow/list.js
+++ web/src/main/webapp/statics/js/cashflow/list.js
@@ -1,15 +1,15 @@
-myApp.onPageInit('cashflow.index', function(page) {
-			cashflow_index_stats();
+myApp.onPageInit('cashflow.list', function(page) {
+			cashflow_list_stats();
 		});
 
-function cashflow_index_stats() {
+function cashflow_list_stats() {
 	$$.get(appUrl + '/cashflow/stats.htm', {
 				type : 'detail'
 			}, function(data) {
 				var stats = data.split("&");
-				$$('#cashflow/index/a').html(stats[0]);
-				$$('#cashflow/index/c').html(stats[1]);
-				$$('#cashflow/index/b').html(stats[2]);
-				$$('#cashflow/index/curBal').html(stats[3]);
+				$$('#cashflow/list/a').html(stats[0]);
+				$$('#cashflow/list/c').html(stats[1]);
+				$$('#cashflow/list/b').html(stats[2]);
+				$$('#cashflow/list/curBal').html(stats[3]);
 			});
 }
--- web/src/main/webapp/statics/js/sale/list.js
+++ web/src/main/webapp/statics/js/sale/list.js
@@ -5,7 +5,7 @@ myApp.onPageInit('sale.list', function(page) {
 function sale_list_stats() {
 	$$.get(appUrl + '/sale/stats.htm', {}, function(data) {
 				var stats = data.split("&");
-				$$('#sale/index/a').html(stats[0]);
-				$$('#sale/index/b').html(stats[1]);
+				$$('#sale/list/a').html(stats[0]);
+				$$('#sale/list/b').html(stats[1]);
 			});
 }
